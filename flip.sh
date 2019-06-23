f906c7e5eabfc1c183ead658d5d7bfa1b6229ed0
chandratech214 <chandratech214@gmail.com>
2019-06-23 13:15:55 +0530

    Revert "hello.sh flip.sh"
    This reverts commit 35479de85fdf0acf3acfd4c17d274df9449bdc0d.

--- flip.sh
+++
@@ -1,2 +0,0 @@
-Hi this is my 2nd order and it is a flipcover for mobile.
-I bought two flipcovers one for moto e2 and the other for redmi 6a.
--- hello.sh
+++
@@ -1,3 +0,0 @@
-hi this is my first order from flipkart.
-It is a mobile manufactured by motorola.
-Model name is moto e2 2nd gen which supports 4g network.
